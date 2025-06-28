1df4db40ab16487746ec582252ffeaa857923872
Adam Powers <apowers@ato.ms>
2025-06-28 06:55:40 -0700

    fix ts errors, add type exports

--- layout.ts
+++ layout.ts
@@ -15,13 +15,13 @@
  */
 
 // Type definitions
-type Node = string | number;
-type Edge = [Node, Node];
-type Graph = {
+export type Node = string | number;
+export type Edge = [Node, Node];
+export type Graph = {
   nodes?: () => Node[];
   edges?: () => Edge[];
   getEdgeData?: (source: Node, target: Node, attr: string) => any;
-} | Node[];
+};
 
 type Position = number[];
 type PositionMap = Record<Node, Position>;
